3d04a1ce2348ac7c42b4caa529e2b2f69e136470
bryan navarro <br.navarroa@duocuc.cl>
2024-04-10 00:24:04 +0000

    Evaluación N°1 DRY7122 BN

---
+++ PRUEBA1_BN.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/Documents
+chmod +x PRUEBA1_BN
+sudo chown root:root PRUEBA1_BN
+echo "El archivo ha sido configurado por bryan navarro para ejecutarse y su propiedad se a cambiado a root."
